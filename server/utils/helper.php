17692bc3c640110561125bfa97d445a63e50b257
Selpol <warfcevip@gmail.com>
2023-09-05 16:24:02 +0300

    Refactoring code

--- server/utils/helper.php
+++ server/utils/helper.php
@@ -8,6 +8,7 @@ use Selpol\Container\Container;
 use Selpol\Kernel\Kernel;
 use Selpol\Logger\FileLogger;
 use Selpol\Service\BackendService;
+use Selpol\Service\RedisService;
 use Selpol\Task\Task;
 use Selpol\Task\TaskContainer;
 use Selpol\Validator\Validator;
@@ -130,9 +131,9 @@ if (!function_exists('check_string')) {
 if (!function_exists('clear_cache')) {
     function clear_cache($uid): int
     {
-        global $redis;
-
         $keys = [];
+
+        $redis = container(RedisService::class)->getRedis();
 
         if ($uid === true)
             $keys = $redis->keys("cache_*");
