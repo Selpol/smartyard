78dce06249422c903aa4e1eeeb693e2107150e0c
Selpol <warfcevip@gmail.com>
2023-09-05 16:30:03 +0300

    Refactoring code

--- server/frontend.php
+++ server/frontend.php
@@ -109,7 +109,7 @@ $clearCache = false;
 
 function response(int $code = 204, mixed $data = false)
 {
-    global $params, $backends, $db;
+    global $db;
 
     $db = null;
 
@@ -117,21 +117,11 @@ function response(int $code = 204, mixed $data = false)
     http_response_code($code);
 
     if ($code == 204) {
-        $backends["accounting"]->log($params, $code);
-
         exit;
     }
 
     if ($data)
         echo json_encode($data, JSON_UNESCAPED_UNICODE);
-
-    if ($backends && $backends["accounting"]) $backends["accounting"]->log($params, $code);
-    else {
-        $login = @($params["_login"] ?: $params["login"]);
-        $login = $login ?: "-";
-
-        error_log("{$_SERVER['REMOTE_ADDR']}:{$_SERVER['REMOTE_PORT']} [$code]: {$_SERVER['REQUEST_METHOD']} $login {$_SERVER["REQUEST_URI"]}");
-    }
 
     exit;
 }
