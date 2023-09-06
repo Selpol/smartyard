0d34b6c59c67a9d60012021e868487cfd4597c20
Selpol <warfcevip@gmail.com>
2023-09-06 10:07:57 +0300

    Refactoring code

--- server/kernel/Runner/RouterRunner.php
+++ server/kernel/Runner/RouterRunner.php
@@ -130,7 +130,7 @@ class RouterRunner implements KernelRunner, RequestHandlerInterface
 
                 $response = container(HttpService::class)
                     ->createResponse(500)
-                    ->withJson(['code' => 500, 'name' => Response::$codes[500]['name'], 'message' => 'Внутренняя ошибка сервера']);
+                    ->withJson(['code' => 500, 'name' => Response::$codes[500]['name'], 'message' => Response::$codes[500]['message']]);
             }
 
             return $this->emit($response);
@@ -143,59 +143,65 @@ class RouterRunner implements KernelRunner, RequestHandlerInterface
 
     private function emit(ResponseInterface $response): int
     {
-        header('HTTP/' . $response->getProtocolVersion() . ' ' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
+        try {
+            header('HTTP/' . $response->getProtocolVersion() . ' ' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
 
-        foreach ($response->getHeaders() as $name => $values)
-            header($name . ': ' . $response->getHeaderLine($name), false);
+            foreach ($response->getHeaders() as $name => $values)
+                header($name . ': ' . $response->getHeaderLine($name), false);
 
-        if ($response->getStatusCode() != 204) {
-            $body = $response->getBody();
+            if ($response->getStatusCode() != 204) {
+                $body = $response->getBody();
 
-            if ($body->getSize() > 1024 * 1024) {
-                $begin = 0;
-                $size = $body->getSize();
-                $end = $size - 1;
+                if ($body->getSize() > 1024 * 1024) {
+                    $begin = 0;
+                    $size = $body->getSize();
+                    $end = $size - 1;
 
-                if (isset($_SERVER['HTTP_RANGE'])) {
-                    if (preg_match('/bytes=\h*(\d+)-(\d*)[\D.*]?/i', $_SERVER['HTTP_RANGE'], $matches)) {
-                        $begin = intval($matches[1]);
-                        if (!empty($matches[2]))
-                            $end = intval($matches[2]);
+                    if (isset($_SERVER['HTTP_RANGE'])) {
+                        if (preg_match('/bytes=\h*(\d+)-(\d*)[\D.*]?/i', $_SERVER['HTTP_RANGE'], $matches)) {
+                            $begin = intval($matches[1]);
+                            if (!empty($matches[2]))
+                                $end = intval($matches[2]);
+                        }
+
+                        header('HTTP/1.1 206 Partial Content');
+                        header("Content-Range: bytes $begin-$end/$size");
+                    } else
+                        header('HTTP/1.1 200 OK');
+
+                    $new_length = $end - $begin + 1;
+
+                    header('Cache-Control: public, must-revalidate, max-age=0');
+                    header('Pragma: no-cache');
+                    header('Accept-Ranges: bytes');
+                    header('Content-Length:' . $new_length);
+                    header('Content-Transfer-Encoding: binary');
+
+                    $chunk_size = 1024 * 1024;
+                    $bytes_send = 0;
+
+                    if (isset($_SERVER['HTTP_RANGE']))
+                        $body->seek($begin);
+
+                    while (!$body->eof() && !connection_aborted() && ($bytes_send < $new_length)) {
+                        $buffer = $body->read($chunk_size);
+
+                        echo $buffer;
+
+                        $bytes_send += strlen($buffer);
                     }
+                } else {
+                    header('Content-Length: ' . $body->getSize());
 
-                    header('HTTP/1.1 206 Partial Content');
-                    header("Content-Range: bytes $begin-$end/$size");
-                } else
-                    header('HTTP/1.1 200 OK');
-
-                $new_length = $end - $begin + 1;
-
-                header('Cache-Control: public, must-revalidate, max-age=0');
-                header('Pragma: no-cache');
-                header('Accept-Ranges: bytes');
-                header('Content-Length:' . $new_length);
-                header('Content-Transfer-Encoding: binary');
-
-                $chunk_size = 1024 * 1024;
-                $bytes_send = 0;
-
-                if (isset($_SERVER['HTTP_RANGE']))
-                    $body->seek($begin);
-
-                while (!$body->eof() && !connection_aborted() && ($bytes_send < $new_length)) {
-                    $buffer = $body->read($chunk_size);
-
-                    echo $buffer;
-
-                    $bytes_send += strlen($buffer);
+                    echo $body->getContents();
                 }
-            } else {
-                header('Content-Length: ' . $body->getSize());
 
-                echo $body->getContents();
+                $body->close();
             }
+        } catch (Throwable $throwable) {
+            logger('response')->emergency('Emergency error' . PHP_EOL . $throwable);
 
-            $body->close();
+            echo '{ "code": 500, "message": "Внутренняя ошибка сервера" }';
         }
 
         return 0;
