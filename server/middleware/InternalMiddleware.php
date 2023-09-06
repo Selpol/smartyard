0c7fc9c208db2dffbad7e4155cff5d74f90d474f
Selpol <warfcevip@gmail.com>
2023-09-06 10:49:37 +0300

    Add logger to InternalMiddleware

--- server/middleware/InternalMiddleware.php
+++ server/middleware/InternalMiddleware.php
@@ -6,21 +6,26 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Log\LoggerInterface;
 use Selpol\Http\Response;
 use Selpol\Service\HttpService;
 
 class InternalMiddleware implements MiddlewareInterface
 {
     private array $trust;
+    private ?LoggerInterface $logger;
 
     public function __construct()
     {
         $this->trust = config('internal.trust') ?? ['127.0.0.1/32'];
+        $this->logger = (config('internal.logger') ?? false) ? logger('internal') : null;
     }
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $ip = $request->getHeader('X-Real-Ip');
+
+        $this->logger?->debug('Request ' . $request->getRequestTarget(), ['ip' => $ip]);
 
         if (count($ip) === 0) {
             /** @var HttpService $http */
@@ -31,8 +36,13 @@ class InternalMiddleware implements MiddlewareInterface
         }
 
         foreach ($this->trust as $item)
-            if ($this->ipInRange($ip[0], $item))
-                return $handler->handle($request);
+            if ($this->ipInRange($ip[0], $item)) {
+                $response = $handler->handle($request);
+
+                $this->logger?->debug('Response ' . $request->getRequestTarget(), ['ip' => $ip, 'code' => $response->getStatusCode()]);
+
+                return $response;
+            }
 
         /** @var HttpService $http */
         $http = $request->getAttribute('http');
