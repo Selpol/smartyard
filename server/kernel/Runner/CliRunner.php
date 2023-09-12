aecfc3363c5c7c3efc2207eb066266d488c3c9d9
Selpol <warfcevip@gmail.com>
2023-09-12 17:32:23 +0300

    Refactoring code

--- server/kernel/Runner/CliRunner.php
+++ server/kernel/Runner/CliRunner.php
@@ -7,7 +7,6 @@ use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\NotFoundExceptionInterface;
 use Psr\Log\LoggerInterface;
 use Psr\SimpleCache\InvalidArgumentException;
-use RuntimeException;
 use Selpol\Cache\FileCache;
 use Selpol\Container\ContainerConfigurator;
 use Selpol\Kernel\Kernel;
@@ -42,17 +41,6 @@ class CliRunner implements KernelRunner
      */
     function __invoke(Kernel $kernel): int
     {
-        try {
-            $device = intercom('iscomx1', 'http://10.101.1.244', '123456');
-
-            if (!$device->ping())
-                throw new RuntimeException();
-        } catch (Throwable $throwable) {
-            echo $throwable;
-        }
-
-        return 0;
-
         $arguments = $this->getArguments();
 
         if ($this->isCommand($arguments, '--init-db')) $this->initDb();
