773f941610db902a5bfe3564a8eee3f4b24cfab1
Selpol <warfcevip@gmail.com>
2023-09-13 13:56:33 +0300

    Add Fake Camera

--- server/device/Ip/Camera/CameraModel.php
+++ server/device/Ip/Camera/CameraModel.php
@@ -3,6 +3,7 @@
 namespace Selpol\Device\Ip\Camera;
 
 use Selpol\Device\Ip\Camera\Beward\BewardCamera;
+use Selpol\Device\Ip\Camera\Fake\FakeCamera;
 use Selpol\Device\Ip\Camera\Is\IsCamera;
 
 class CameraModel
@@ -46,7 +47,8 @@ class CameraModel
         if (!isset(self::$models))
             self::$models = [
                 'is' => new CameraModel('IS DOMOPHONE CAMERA', 'IS', IsCamera::class),
-                'beward' => new CameraModel('BEWARD DOMOPHONE CAMERA', 'BEWARD', BewardCamera::class)
+                'beward' => new CameraModel('BEWARD DOMOPHONE CAMERA', 'BEWARD', BewardCamera::class),
+                'fake' => new CameraModel('FAKE CAMERA', 'FAKE', FakeCamera::class)
             ];
 
         return self::$models;
---
+++ server/device/Ip/Camera/Fake/FakeCamera.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Selpol\Device\Ip\Camera\Fake;
+
+use Selpol\Device\Ip\Camera\CameraDevice;
+
+class FakeCamera extends CameraDevice
+{
+
+}
