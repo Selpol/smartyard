2add92cb76d4e0e886a34f65af45495d7d833000
Selpol <warfcevip@gmail.com>
2023-09-14 16:27:18 +0300

    Fix IntercomController syns flat task

--- server/controller/mobile/IntercomController.php
+++ server/controller/mobile/IntercomController.php
@@ -94,7 +94,7 @@ class IntercomController extends Controller
                 $households->modifySubscriber($user['subscriberId'], $params);
             }
 
-            dispatch_high(new IntercomSyncFlatTask($validate['flatId']));
+            dispatch_high(new IntercomSyncFlatTask($validate['flatId'], false));
         }
 
         $subscriber = $households->getSubscribers('id', $user['subscriberId'])[0];
@@ -234,7 +234,7 @@ class IntercomController extends Controller
         $households->modifyFlat($flat_id, $params);
         $flat = $households->getFlat($flat_id);
 
-        dispatch_high(new IntercomSyncFlatTask($validate['flatId']));
+        dispatch_high(new IntercomSyncFlatTask($validate['flatId'], false));
 
         return $this->rbtResponse(200, ["code" => intval($flat['openCode'])]);
     }
