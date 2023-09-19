e2b3dd3c6b47ad91f27d9e0e38137a685ec22b6e
Selpol <warfcevip@gmail.com>
2023-09-19 11:39:02 +0300

    Refactroing code

--- server/controller/Internal/SyncController.php
+++ server/controller/Internal/SyncController.php
@@ -137,7 +137,7 @@ class SyncController extends Controller
                 $validate = validator($item, ['subscriber' => [Rule::id()], 'flat' => [Rule::id()], 'role' => [Rule::required(), Rule::int(), Rule::nonNullable()]]);
 
                 if ($db->insert('INSERT INTO houses_flats_subscribers(house_subscriber_id, house_flat_id, role) VALUES (:subscriber_id, :flat_id, :role)', ['subscriber_id' => $validate['subscriber'], 'flat_id' => $validate['flat'], 'role' => $validate['role']]))
-                    $result[$validate['subscriber'] . $validate['flat']] = true;
+                    $result[$validate['subscriber'] . '-' . $validate['flat']] = true;
             } catch (Throwable $throwable) {
                 logger('internal-sync')->error($throwable);
             }
@@ -165,7 +165,7 @@ class SyncController extends Controller
                 $validate = validator($item, ['subscriber' => [Rule::id()], 'flat' => [Rule::id()]]);
 
                 if ($db->modify('DELETE FROM houses_flats_subscribers WHERE house_subscriber_id = :subscriber_id AND house_flat_id = :flat_id', ['subscriber_id' => $validate['subscriber'], 'flat_id' => $validate['flat']]))
-                    $result[$validate['subscriber'] . $validate['flat']] = true;
+                    $result[$validate['subscriber'] . '-' . $validate['flat']] = true;
             } catch (Throwable $throwable) {
                 logger('internal-sync')->error($throwable);
             }
