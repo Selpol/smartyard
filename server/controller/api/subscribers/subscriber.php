0c1830c352295c277c77d09c1c96660d818d6484
Selpol <warfcevip@gmail.com>
2023-09-26 12:35:46 +0300

    Refactoring code

--- server/controller/api/subscribers/subscriber.php
+++ server/controller/api/subscribers/subscriber.php
@@ -30,7 +30,7 @@ namespace api\subscribers {
         {
             $households = container(HouseFeature::class);
 
-            $subscriberId = $households->addSubscriber($params["mobile"], $params["subscriberName"], $params["subscriberPatronymic"], null, @$params["flatId"], @$params["message"]);
+            $subscriberId = $households->addSubscriber($params["mobile"], @$params["subscriberName"], @$params["subscriberPatronymic"], null, @$params["flatId"], @$params["message"]);
 
             return api::ANSWER($subscriberId, ($subscriberId !== false) ? "subscriber" : false);
         }
