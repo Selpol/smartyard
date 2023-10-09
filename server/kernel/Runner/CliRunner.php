e83634ee7905ee304c7d3b53388dff4b0962fce8
Selpol <warfcevip@gmail.com>
2023-10-09 15:45:38 +0300

    Refactoring code

--- server/kernel/Runner/CliRunner.php
+++ server/kernel/Runner/CliRunner.php
@@ -12,7 +12,6 @@ use RedisException;
 use Selpol\Cache\FileCache;
 use Selpol\Container\ContainerConfigurator;
 use Selpol\Entity\Model\Permission;
-use Selpol\Entity\Repository\AuditRepository;
 use Selpol\Entity\Repository\PermissionRepository;
 use Selpol\Feature\Audit\AuditFeature;
 use Selpol\Feature\Frs\FrsFeature;
@@ -52,8 +51,6 @@ class CliRunner implements KernelRunner
      */
     function __invoke(Kernel $kernel): int
     {
-        var_dump(container(AuditRepository::class)->fetchPaginate(2, 10));
-
         chdir(path(''));
 
         $arguments = $this->getArguments();
