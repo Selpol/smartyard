cbd231e7605bc7aa57a60ef75ec27d7da1bfded4
Selpol <warfcevip@gmail.com>
2023-10-09 13:58:19 +0300

    Add default value to keys page

--- server/controller/api/key/keys.php
+++ server/controller/api/key/keys.php
@@ -4,6 +4,7 @@ namespace api\key;
 
 use api\api;
 use Selpol\Entity\Repository\House\HouseKeyRepository;
+use Selpol\Validator\Filter;
 use Selpol\Validator\Rule;
 
 class keys extends api
@@ -11,8 +12,8 @@ class keys extends api
     public static function GET($params)
     {
         $validate = validator($params, [
-            'page' => [Rule::int(), Rule::min(0), Rule::max()],
-            'size' => [Rule::int(), Rule::min(0), Rule::max(1000)]
+            'page' => [Filter::default(0), Rule::int(), Rule::min(0), Rule::max()],
+            'size' => [Filter::default(10), Rule::int(), Rule::min(0), Rule::max(1000)]
         ]);
 
         return self::SUCCESS('keys', container(HouseKeyRepository::class)->fetchPaginate($validate['page'], $validate['size']));
