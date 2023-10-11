34d44c8e27638bddc39b0f2c52e171e6d0938bd0
Selpol <warfcevip@gmail.com>
2023-10-11 12:20:54 +0300

    Update frontend api keys

--- server/controller/api/key/keys.php
+++ server/controller/api/key/keys.php
@@ -4,6 +4,8 @@ namespace api\key;
 
 use api\api;
 use Selpol\Entity\Repository\House\HouseKeyRepository;
+use Selpol\Service\Database\Page;
+use Selpol\Service\DatabaseService;
 use Selpol\Validator\Filter;
 use Selpol\Validator\Rule;
 
@@ -19,7 +21,28 @@ class keys extends api
             'size' => [Filter::default(10), Rule::int(), Rule::min(0), Rule::max(1000)]
         ]);
 
-        return self::SUCCESS('keys', container(HouseKeyRepository::class)->fetchPaginate($validate['page'], $validate['size'], criteria()->like('rfid', $validate['rfid'])->orLike('comments', $validate['comments'])->asc('house_rfid_id')));
+        $page = container(HouseKeyRepository::class)->fetchPaginate($validate['page'], $validate['size'], criteria()->like('rfid', $validate['rfid'])->orLike('comments', $validate['comments'])->asc('house_rfid_id')->asc('access_to'));
+        $data = $page->getData();
+
+        $flats = [];
+        $houses = [];
+
+        $db = container(DatabaseService::class);
+
+        foreach ($data as $key) {
+            if ($key->access_type === 2) {
+                if (!array_key_exists($key->access_to, $flats))
+                    $flats[$key->access_to] = $db->get('SELECT address_house_id FROM houses_flats WHERE house_flat_id = :house_flat_id', ['house_flat_id' => $key->access_to], options: ['singlify'])['address_house_id'];
+
+                if (!array_key_exists($flats[$key->access_to], $houses))
+                    $houses[$flats[$key->access_to]] = $db->get('SELECT house_full FROM addresses_houses WHERE address_house_id = :address_house_id', ['address_house_id' => $flats[$key->access_to]], options: ['singlify'])['house_full'];
+
+                $key->house_id = $flats[$key->access_to];
+                $key->house_address = $houses[$flats[$key->access_to]];
+            }
+        }
+
+        return self::SUCCESS('keys', new Page($data, $page->getTotal(), $page->getPage(), $page->getSize()));
     }
 
     public static function index(): array
