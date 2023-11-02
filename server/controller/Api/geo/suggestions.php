cb5e6e058cf6e4aefcefeec1c1a01bd955e6f818
Руслан Худобородов <warfcevip@gmail.com>
2023-11-02 15:14:48 +0300

    Update suggestions

--- server/controller/Api/geo/suggestions.php
+++ server/controller/Api/geo/suggestions.php
@@ -2,17 +2,20 @@
 
 namespace Selpol\Controller\Api\geo;
 
+use Psr\Http\Message\ResponseInterface;
 use Selpol\Controller\Api\Api;
 use Selpol\Feature\Geo\GeoFeature;
 
 readonly class suggestions extends Api
 {
-
-    public static function GET(array $params): array
+    public static function GET(array $params): ResponseInterface
     {
         $suggestions = container(GeoFeature::class)->suggestions($params["search"]);
 
-        return Api::ANSWER($suggestions, ($suggestions !== false) ? "suggestions" : "404");
+        if ($suggestions)
+            return self::success($suggestions);
+
+        return self::error('Адрес не найден', 404);
     }
 
     public static function index(): bool|array
