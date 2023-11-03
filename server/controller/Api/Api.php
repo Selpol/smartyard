acfb46e68a75b1b1ec99aa977d4cbdc1e5701805
Selpol <warfcevip@gmail.com>
2023-11-03 15:18:26 +0300

    Remove unused Api methods

--- server/controller/Api/Api.php
+++ server/controller/Api/Api.php
@@ -13,95 +13,22 @@ readonly abstract class Api
 
     public static function GET(array $params): array|Response|ResponseInterface
     {
-        return self::ANSWER(false, 'badRequest');
+        return self::error('Метода не существует', 404);
     }
 
     public static function POST(array $params): array|Response|ResponseInterface
     {
-        return self::ANSWER(false, 'badRequest');
+        return self::error('Метода не существует', 404);
     }
 
     public static function PUT(array $params): array|Response|ResponseInterface
     {
-        return self::ANSWER(false, 'badRequest');
+        return self::error('Метода не существует', 404);
     }
 
     public static function DELETE(array $params): array|Response|ResponseInterface
     {
-        return self::ANSWER(false, 'badRequest');
-    }
-
-    /**
-     * sends templated answer or error
-     *
-     * $result - sends error or success
-     * if false sends error with error code $answer
-     * if true sends json with parent $answer
-     * with default params returns 204
-     *
-     * @param mixed $result
-     * @param integer|boolean|array|string $answer
-     * @param integer $cache
-     * @return array
-     */
-    public static function ANSWER(mixed $result = true, int|bool|array|string $answer = false, int $cache = -1): array
-    {
-        if ($result === false)
-            return self::FALSE($answer);
-        else if (is_int($answer) || is_bool($answer) || is_string($answer))
-            return self::TRUE($answer, $result, $cache);
-        else if (!$answer)
-            return self::TRUE(true, false, $cache);
-
-        return self::FALSE('unknown');
-    }
-
-    /**
-     * more specific (success only) return function
-     *
-     * @param string|bool|int $key
-     * @param mixed $data
-     * @param integer $cache
-     *
-     * @return array[]
-     */
-    public static function TRUE(string|bool|int $key, mixed $data, int $cache = -1): array
-    {
-        global $redis_cache_ttl;
-
-        if ($data !== false)
-            $r = ['200' => [$key => $data]];
-        else
-            $r = ['204' => false];
-
-        if ($cache < 0)
-            $cache = $redis_cache_ttl;
-
-        $r[] = ['cache' => $cache];
-
-        return $r;
-    }
-
-    /**
-     * more specific (error only) return function
-     *
-     * @param bool|int|string $error
-     * @return array
-     */
-    public static function FALSE(bool|int|string $error = ''): array
-    {
-        if (!$error) {
-            $error = last_error();
-
-            if (!$error)
-                $error = 'unknown';
-        }
-
-        $errors = ['badRequest' => 400, 'forbidden' => 403, 'notFound' => 404, 'notAcceptable' => 406, 'unknown' => 418];
-
-        $code = array_key_exists($error, $errors) ? $errors[$error] : 400;
-
-        return [$code => ['error' => array_key_exists($code, Response::$codes) ? Response::$codes[$code]['message'] : $error]];
+        return self::error('Метода не существует', 404);
     }
 
     public static function success(mixed $data = null, int $code = 200): ResponseInterface
