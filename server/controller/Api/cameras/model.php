17fc64ba1736d02277af9f800f9c571e5b1aa21a
Selpol <warfcevip@gmail.com>
2023-11-10 14:23:52 +0300

    Fix api frontend cameras model

--- server/controller/Api/cameras/model.php
+++ server/controller/Api/cameras/model.php
@@ -4,11 +4,16 @@ namespace Selpol\Controller\Api\cameras;
 
 use Psr\Http\Message\ResponseInterface;
 use Selpol\Controller\Api\Api;
-use Selpol\Framework\Http\Response;
+use Selpol\Device\Ip\Camera\CameraModel;
 
 readonly class model extends Api
 {
-    public static function GET(array $params): array|Response|ResponseInterface
+    public static function GET(array $params): ResponseInterface
+    {
+        return self::success(CameraModel::models());
+    }
+
+    public static function index(): array|bool
     {
         return ['GET' => '[Камера] Получить список моделей'];
     }
