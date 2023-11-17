571a607c53d572e804e8b3ac333fd1fa080d8168
Selpol <warfcevip@gmail.com>
2023-11-17 16:23:23 +0300

    Update api frontend dvr

--- server/controller/Api/server/dvr.php
+++ server/controller/Api/server/dvr.php
@@ -26,11 +26,9 @@ readonly class dvr extends Api
 
             'url' => rule()->required()->url()->nonNullable(),
 
-            'token' => rule()->required()->string()->max(1024)->nonNullable()
+            'token' => rule()->required()->string()->max(1024)->nonNullable(),
+            'credentials' => rule()->required()->string()->max(1024)->nonNullable()
         ]));
-
-        if (array_key_exists('credentials', $params))
-            $dvrServer->credentials = rule()->string()->max(1024)->onItem('credentials', $params);
 
         if ($dvrServer->insert())
             return self::success($dvrServer->id);
