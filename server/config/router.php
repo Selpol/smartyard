41669445d2cff1cf918bf32ce59181eea8309947
Selpol <warfcevip@gmail.com>
2023-12-20 08:22:15 +0300

    Remove PrometheusMiddleware

--- server/config/router.php
+++ server/config/router.php
@@ -3,12 +3,9 @@
 use Selpol\Framework\Router\RouterConfigurator;
 use Selpol\Middleware\Mobile\AuthMiddleware;
 use Selpol\Middleware\Mobile\SubscriberMiddleware;
-use Selpol\Middleware\PrometheusMiddleware;
 use Selpol\Middleware\RateLimitMiddleware;
 
 return static function (RouterConfigurator $configurator) {
-    $configurator->include('', PrometheusMiddleware::class);
-
     $configurator->include('/mobile', AuthMiddleware::class);
     $configurator->include('/mobile', SubscriberMiddleware::class);
     $configurator->include('/mobile', RateLimitMiddleware::class, ['trust' => config_get('mobile.trust', ['127.0.0.1/32']), 'count' => 60, 'ttl' => 60]);
