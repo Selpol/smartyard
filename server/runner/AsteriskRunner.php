e51c3f83409e51c147cb79ed429d4787533e39ba
Selpol <warfcevip@gmail.com>
2024-01-16 11:56:43 +0300

    Add AsteriskiRunner get flat domophone ip

--- server/runner/AsteriskRunner.php
+++ server/runner/AsteriskRunner.php
@@ -86,8 +86,16 @@ class AsteriskRunner implements RunnerInterface, RunnerExceptionHandlerInterface
 
                         $flat = $households->getFlat(intval($params));
 
-                        if ($flat['autoBlock'] == 0 && $flat['adminBlock'] == 0 && $flat['manualBlock'] == 0)
+                        if ($flat['autoBlock'] == 0 && $flat['adminBlock'] == 0 && $flat['manualBlock'] == 0) {
+                            try {
+                                for ($i = 0; $i < count($flat['entrances']); $i++)
+                                    $flat['entrances'][$i]['domophoneIp'] = DeviceIntercom::findById($flat['entrances'][$i]['domophoneId'], setting: setting()->columns(['ip']))?->ip ?? null;
+                            } catch (Throwable $throwable) {
+                                $this->logger->debug($throwable);
+                            }
+
                             echo json_encode($flat);
+                        }
 
                         $this->logger->debug('Get flat', ['flat' => $flat, 'params' => $params]);
 
