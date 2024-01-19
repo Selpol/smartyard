826d18fcc13842d23b9aeb6fcf972a230adbff18
Selpol <warfcevip@gmail.com>
2024-01-19 15:09:34 +0300

    Update StatusController

--- server/controller/Internal/StatusController.php
+++ server/controller/Internal/StatusController.php
@@ -8,14 +8,13 @@ use Selpol\Controller\RbtController;
 use Selpol\Entity\Model\Core\CoreVar;
 use Selpol\Framework\Router\Attribute\Controller;
 use Selpol\Framework\Router\Attribute\Method\Get;
-use Selpol\Middleware\RateLimitMiddleware;
 use Selpol\Service\TaskService;
 use Throwable;
 
-#[Controller('/internal/zabbix')]
-readonly class ZabbixController extends RbtController
+#[Controller('/internal/status')]
+readonly class StatusController extends RbtController
 {
-    #[Get(includes: [RateLimitMiddleware::class => ['trust' => [], 'count' => 1, 'ttl' => 30, 'request' => true]])]
+    #[Get]
     public function index(RedisCache $cache, TaskService $task): ResponseInterface
     {
         try {
@@ -26,10 +25,10 @@ readonly class ZabbixController extends RbtController
         }
 
         try {
-            if (!$cache->set('zabbix', 1, 5))
+            if (!$cache->set('status', 1, 5))
                 return user_response(500, message: 'Не удалось закэшировать значение');
 
-            if ($cache->get('zabbix') != 1)
+            if ($cache->get('status') != 1)
                 return user_response(500, message: 'Не удалось получить закэшированное значение');
         } catch (Throwable) {
             return user_response(500, message: 'Redis сервер не доступен');
