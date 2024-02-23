fab4020bc4ce5353ab004179f3b65267cb69da45
Руслан Худобородов <warfcevip@gmail.com>
2024-02-23 21:37:26 +0300

    Fix login api

--- server/controller/Api/authentication/login.php
+++ server/controller/Api/authentication/login.php
@@ -3,6 +3,7 @@
 namespace Selpol\Controller\Api\authentication;
 
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Selpol\Controller\Api\Api;
 use Selpol\Feature\Authentication\AuthenticationFeature;
 
@@ -10,7 +11,7 @@ readonly class login extends Api
 {
     public static function POST(array $params): ResponseInterface
     {
-        $auth = container(AuthenticationFeature::class)->login($params["login"], $params["password"], $params["rememberMe"] && $params["ua"] && $params["did"], trim($params["ua"]), trim($params["did"]));
+        $auth = container(AuthenticationFeature::class)->login($params["login"], $params["password"], $params["rememberMe"] && $params["ua"] && $params["did"], trim($params["ua"]), trim($params["did"]), connection_ip(container(ServerRequestInterface::class)));
 
         if ($auth['result'])
             return self::success($auth['token']);
