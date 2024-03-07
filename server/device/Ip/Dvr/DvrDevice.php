4f58a9b44a159c6ea684967f6ecfaf34307a4179
Ruslan KHudoborodov <warfcevip@gmail.com>
2024-03-07 12:20:24 +0300

    Refactoring code

--- server/device/Ip/Dvr/DvrDevice.php
+++ server/device/Ip/Dvr/DvrDevice.php
@@ -3,7 +3,6 @@
 namespace Selpol\Device\Ip\Dvr;
 
 use Selpol\Device\Ip\IpDevice;
-use Selpol\Entity\Model\Device\DeviceCamera;
 use Selpol\Framework\Http\Uri;
 use SensitiveParameter;
 
@@ -28,20 +27,5 @@ abstract class DvrDevice extends IpDevice
     public function getCameraId(string $query): ?string
     {
         return null;
-    }
-
-    public function addCamera(DeviceCamera $camera): bool
-    {
-        return true;
-    }
-
-    public function updateCamera(DeviceCamera $camera): bool
-    {
-        return true;
-    }
-
-    public function removeCamera(DeviceCamera $camera): bool
-    {
-        return true;
     }
 }
