5a34f52e6052374ee0725c542551bceb8b7eb5c4
Ruslan KHudoborodov <warfcevip@gmail.com>
2024-03-13 15:10:05 +0300

    Updat esubscribers frontend api

--- server/controller/Api/subscribers/subscribers.php
+++ server/controller/Api/subscribers/subscribers.php
@@ -4,12 +4,26 @@ namespace Selpol\Controller\Api\subscribers;
 
 use Psr\Http\Message\ResponseInterface;
 use Selpol\Controller\Api\Api;
+use Selpol\Entity\Model\House\HouseSubscriber;
 use Selpol\Feature\House\HouseFeature;
 
 readonly class subscribers extends Api
 {
     public static function GET(array $params): ResponseInterface
     {
+        if ($params['by'] === 'name') {
+            $validate = validator($params, [
+                'name' => rule()->string()->clamp(0, 1000),
+
+                'page' => [filter()->default(0), rule()->required()->int()->clamp(0)->nonNullable()],
+                'size' => [filter()->default(10), rule()->required()->int()->clamp(1, 1000)->nonNullable()]
+            ]);
+
+            $criteria = criteria()->like('subscriber_name', $validate['name'])->orLike('subscriber_patronymic', $validate['name'])->asc('house_subscriber_id');
+
+            return self::success(HouseSubscriber::fetchPage($validate['page'], $validate['size'], $criteria, setting()->columns(['house_subscriber_id', 'subscriber_name', 'subscriber_patronymic'])));
+        }
+
         $households = container(HouseFeature::class);
 
         $flat = [
