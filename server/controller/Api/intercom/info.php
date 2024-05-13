b37e842902531c25ad2d7da85830cc1b768d061e
Ruslan KHudoborodov <warfcevip@gmail.com>
2024-05-13 09:21:19 +0300

    Update intercom info api

--- server/controller/Api/intercom/info.php
+++ server/controller/Api/intercom/info.php
@@ -4,16 +4,33 @@ namespace Selpol\Controller\Api\intercom;
 
 use Psr\Http\Message\ResponseInterface;
 use Selpol\Controller\Api\Api;
+use Selpol\Entity\Model\Device\DeviceIntercom;
 use Selpol\Framework\Http\Response;
+use Selpol\Service\DeviceService;
+use Throwable;
 
 readonly class info extends Api
 {
     public static function GET(array $params): array|Response|ResponseInterface
     {
-        $intercom = intercom(rule()->id()->onItem('_id', $params));
+        $device = DeviceIntercom::findById(rule()->id()->onItem('_id', $params));
 
-        if ($intercom)
-            return self::success($intercom->getSysInfo());
+        if (!$device)
+            return self::error('Не удалось найти домофон', 404);
+
+        $intercom = container(DeviceService::class)->intercomByEntity($device);
+
+        if ($intercom) {
+            $info = $intercom->getSysInfo();
+
+            try {
+                exec('arp ' . $device->ip . ' | grep ' . $device->ip . ' | awk \'{print $3}\'', $output);
+
+                return self::success(['info' => $info, 'mac' => $output]);
+            } catch (Throwable) {
+                return self::success(['info' => $info, 'mac' => []]);
+            }
+        }
 
         return self::error('Не удалось найти устройство', 404);
     }
