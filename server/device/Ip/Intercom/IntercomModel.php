a5b15f05eb5147a030ac5df4009126ac630cf9bb
Ruslan KHudoborodov <warfcevip@gmail.com>
2024-05-13 09:26:35 +0300

    Add dks977957_2

--- server/device/Ip/Intercom/IntercomModel.php
+++ server/device/Ip/Intercom/IntercomModel.php
@@ -245,6 +245,18 @@ class IntercomModel
                     true,
                     MifareDksIntercom::class
                 ),
+                'dks977957_2' => new IntercomModel(
+                    'BEWARD DKS977957',
+                    'BEWARD',
+                    'DKS977957',
+                    'beward',
+                    'beward',
+                    2,
+                    ['bk-100', 'com-25u', 'com-100u', 'com-220u', 'kad2501', 'kkm-100s2', 'kkm-105', 'km100-7.1', 'km100-7.5', 'kmg-100'],
+                    ['KKM-100S2' => 0, 'KKM-105' => 1, 'KKM-108' => 2, 'KAD2501' => 3, 'KAD2502' => 4],
+                    true,
+                    MifareDksIntercom::class
+                ),
                 'kv6113' => new IntercomModel(
                     'HikVision DS-KV6113',
                     'HIKVISION',
