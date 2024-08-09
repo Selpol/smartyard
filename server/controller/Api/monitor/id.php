5aa9ca3a1bfd628a9a151e85b6d093d4b43ba5c7
Ruslan KHudoborodov <warfcevip@gmail.com>
2024-08-09 13:28:42 +0300

    Refactoring code

--- server/controller/Api/monitor/id.php
+++ server/controller/Api/monitor/id.php
@@ -38,82 +38,76 @@ readonly class id extends Api
      */
     public static function POST(array $params): ResponseInterface
     {
-        try {
-            $type = rule()->in(['ping', 'sip'])->onItem('type', $params);
+        $type = rule()->in(['ping', 'sip'])->onItem('type', $params);
 
-            $result = [];
+        $result = [];
 
-            if ($type === 'sip') {
-                $monitor = container(MonitorFeature::class);
+        if ($type === 'sip') {
+            $monitor = container(MonitorFeature::class);
 
-                foreach ($params['ids'] as $id) {
-                    $id = rule()->id()->onItem('id', ['id' => $id]);
+            foreach ($params['ids'] as $id) {
+                $id = rule()->id()->onItem('id', ['id' => $id]);
 
-                    $result[$id] = $monitor->sip($id);
-                }
-            } else {
-                /** @var Fiber[] $fibers */
-                $fibers = [];
-                $count = count($params['ids']);
+                $result[$id] = $monitor->sip($id);
+            }
+        } else {
+            /** @var Fiber[] $fibers */
+            $fibers = [];
+            $count = count($params['ids']);
 
-                for ($i = 0; $i < $count; $i++)
-                    $fibers[$i] = new Fiber(static function (string $url) {
-                        $timeout = microtime(true);
+            for ($i = 0; $i < $count; $i++)
+                $fibers[$i] = new Fiber(static function (string $url) {
+                    $timeout = microtime(true);
 
-                        $socket = stream_socket_client($url, $errno, $errstr, flags: STREAM_CLIENT_ASYNC_CONNECT);
+                    $socket = stream_socket_client($url, $errno, $errstr, flags: STREAM_CLIENT_ASYNC_CONNECT);
 
-                        if (!$socket)
-                            return false;
+                    if (!$socket)
+                        return false;
 
-                        stream_set_blocking($socket, false);
+                    stream_set_blocking($socket, false);
 
-                        while (true) {
-                            $w = [$socket];
-                            $r = $e = [];
+                    while (true) {
+                        $w = [$socket];
+                        $r = $e = [];
 
-                            if (stream_select($r, $w, $e, 0, 100000)) {
-                                if (microtime(true) - $timeout > 1)
-                                    return false;
+                        if (stream_select($r, $w, $e, 0, 100000)) {
+                            if (microtime(true) - $timeout > 1)
+                                return false;
 
-                                Fiber::suspend();
-                            } else return true;
-                        }
-                    });
-
-                while ($fibers) {
-                    foreach ($fibers as $i => $fiber) {
-                        if (!$fiber->isStarted()) {
-                            $id = rule()->id()->onItem('id', ['id' => $params['ids'][$i]]);
-
-                            $intercom = intercom($id);
-
-                            $url = $intercom->uri->getHost();
-
-                            if ($intercom->uri->getPort() === null) {
-                                $url .= ':' . match (strtolower($intercom->uri->getScheme())) {
-                                        'http' => 80,
-                                        'https' => 443,
-                                        default => 22
-                                    };
-                            } else $url .= ':' . $intercom->uri->getPort();
-
-                            $fiber->start($url);
-                        } else if ($fiber->isTerminated()) {
-                            $result[$params['ids'][$i]] = !$fiber->getReturn();
-
-                            unset($fibers[$i]);
-                        } else if ($fiber->isSuspended())
-                            $fiber->resume();
+                            Fiber::suspend();
+                        } else return true;
                     }
+                });
+
+            while ($fibers) {
+                foreach ($fibers as $i => $fiber) {
+                    if (!$fiber->isStarted()) {
+                        $id = rule()->id()->onItem('id', ['id' => $params['ids'][$i]]);
+
+                        $intercom = intercom($id);
+
+                        $url = $intercom->uri->getHost();
+
+                        if ($intercom->uri->getPort() === null) {
+                            $url .= ':' . match (strtolower($intercom->uri->getScheme())) {
+                                    'http' => 80,
+                                    'https' => 443,
+                                    default => 22
+                                };
+                        } else $url .= ':' . $intercom->uri->getPort();
+
+                        $fiber->start($url);
+                    } else if ($fiber->isTerminated()) {
+                        $result[$params['ids'][$i]] = !$fiber->getReturn();
+
+                        unset($fibers[$i]);
+                    } else if ($fiber->isSuspended())
+                        $fiber->resume();
                 }
             }
-
-            return self::success($result);
-        } catch (Throwable $throwable) {
-            file_logger('monitor')->error($throwable);
-
-            return self::error('Ошибка мониторинга');
         }
+
+        return self::success($result);
     }
 
     public static function index(): array
