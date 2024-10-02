d04569278b8aef7ec3fea820522280c67129b55a
Ruslan KHudoborodov <warfcevip@gmail.com>
2024-10-02 12:17:26 +0300

    Update config api

--- server/controller/Api/config/config.php
+++ server/controller/Api/config/config.php
@@ -7,19 +7,18 @@ use Selpol\Controller\Api\Api;
 use Selpol\Device\Ip\Intercom\IntercomModel;
 use Selpol\Entity\Model\Device\DeviceIntercom;
 use Selpol\Feature\Config\ConfigFeature;
+use Selpol\Service\DatabaseService;
 
 readonly class config extends Api
 {
     public static function GET(array $params): ResponseInterface
     {
         if ($params['_id'] == 'intercom') {
-            $intercoms = DeviceIntercom::fetchAll(setting: setting()->columns(['house_domophone_id', 'device_model']));
-
+            $intercoms = container(DatabaseService::class)->get('SELECT DISTINCT device_model FROM houses_domophones');
             $models = IntercomModel::models();
 
             return self::success([
-                'ids' => array_map(static fn(DeviceIntercom $intercom) => $intercom->house_domophone_id, $intercoms),
-                'models' => array_reduce(array_values(array_unique(array_filter(array_map(static fn(DeviceIntercom $intercom) => $intercom->device_model, $intercoms), static fn(?string $value) => $value !== null && $value != ''))), static function (array $previous, string $current) {
+                'models' => array_reduce(array_values(array_unique(array_filter(array_map('trim', array_map(static fn(array $intercom) => $intercom['device_model'], $intercoms)), static fn(?string $value) => $value !== null && $value != ''))), static function (array $previous, string $current) {
                     if (str_contains($current, '_rev')) {
                         $segments = explode('_rev', $current);
 
