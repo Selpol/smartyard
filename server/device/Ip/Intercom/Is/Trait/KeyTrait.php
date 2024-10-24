984b44f1a33c0480eb7edc1bdd46bf2bded89c5a
Ruslan KHudoborodov <warfcevip@gmail.com>
2024-10-24 13:25:54 +0300

    Update intercom is device

--- server/device/Ip/Intercom/Is/Trait/KeyTrait.php
+++ server/device/Ip/Intercom/Is/Trait/KeyTrait.php
@@ -3,17 +3,22 @@
 namespace Selpol\Device\Ip\Intercom\Is\Trait;
 
 use Selpol\Device\Ip\Intercom\Setting\Key\Key;
+use Throwable;
 
 trait KeyTrait
 {
     public function getKeys(?int $apartment): array
     {
-        $response = $this->get('/key/store', is_null($apartment) ? [] : ['panelCode' => $apartment]);
-        $result = array_map(static fn(array $key): Key => new Key($key['uuid'], $key['panelCode']), $response);
+        try {
+            $response = $this->get('/key/store', is_null($apartment) ? [] : ['panelCode' => $apartment]);
+            $result = array_map(static fn(array $key): Key => new Key($key['uuid'], $key['panelCode']), $response);
 
-        usort($result, static fn(Key $a, Key $b): int => strcmp($a->key, $b->key));
+            usort($result, static fn(Key $a, Key $b): int => strcmp($a->key, $b->key));
 
-        return $result;
+            return $result;
+        } catch (Throwable) {
+            return [];
+        }
     }
 
     public function addKey(Key $key): void
