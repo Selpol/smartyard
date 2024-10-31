3090fb589ba7f9ea46d594b491d7d2a1a4136b2f
Ruslan KHudoborodov <warfcevip@gmail.com>
2024-10-31 09:14:50 +0300

    Refactoring code

---
+++ README.md
@@ -0,0 +1,9 @@
+# SmartYard
+
+Autonomous server part for intercom and video surveillance services.
+
+## Modules
+
+- [Server](./server/README.md)
+- [Syslog](./syslog/README.md)
+- [Asterisk](./asterisk/README.md)
---
+++ asterisk/config.sample.lua
@@ -0,0 +1,11 @@
+redis_server_host = "127.0.0.1"
+redis_server_port = 6379
+redis_server_user = "default"
+redis_server_auth = false
+
+config_rearm = "rbt"
+config_server = "http://127.0.0.1/asterisk/extensions"
+
+config_sos = "PJSIP/112"
+
+log.outfile = "/var/log/asterisk/extensions-lua.log"
