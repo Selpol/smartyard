dfda99b001a4bd71fe5de4018382f2da62672e21
Ruslan KHudoborodov <warfcevip@gmail.com>
2025-02-06 13:56:03 +0300

    Add IntercomHealthTask

--- server/task/Tasks/Intercom/IntercomTask.php
+++ server/task/Tasks/Intercom/IntercomTask.php
@@ -2,18 +2,29 @@
 
 namespace Selpol\Task\Tasks\Intercom;
 
+use Selpol\Device\Ip\Intercom\IntercomDevice;
 use Selpol\Task\Task;
 
 abstract class IntercomTask extends Task
 {
+    private IntercomDevice $device;
+
     protected function __construct(
         /** @var int Идентификатор устройства */
         public int $id,
-        string     $title
-    )
-    {
+        string $title
+    ) {
         parent::__construct($title);
 
         $this->setLogger(file_logger('task-intercom'));
+    }
+
+    protected function getDevice(): IntercomDevice
+    {
+        if (!isset($this->device)) {
+            $this->device = intercom($this->id);
+        }
+
+        return $this->device;
     }
 }
