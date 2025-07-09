c19d29e7cae17856deb24d10f34beebf97fc7509
Ruslan KHudoborodov <warfcevip@gmail.com>
2025-07-09 23:09:22 +0300

    Fix AddressHouseStoreRequest

--- server/controller/Request/Admin/Address/AddressHouseStoreRequest.php
+++ server/controller/Request/Admin/Address/AddressHouseStoreRequest.php
@@ -24,7 +24,7 @@ readonly class AddressHouseStoreRequest extends RouteRequest
             'address_settlement_id' => rule()->int()->clamp(0),
             'address_street_id' => rule()->int()->clamp(0),
 
-            'house_uuid' => rule()->uuid(),
+            'house_uuid' => rule()->uuid4(),
             'house_type' => rule()->required()->string()->nonNullable(),
             'house_type_full' => rule()->string(),
             'house_full' => rule()->required()->string()->nonNullable(),
